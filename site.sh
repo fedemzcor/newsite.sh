c424300c25ab1d99769a0b6a91d9b2c12bdfaafe
Fede Córdova <yofedecordova@gmail.com>
2016-09-09 03:48:20 -0500

    Segunda

--- site.sh
+++ site.sh
@@ -1,2 +1,20 @@
 #! /bin/bash
-clear && read -p "¿Cual es el nombre de usuario que deseas crear? " user_name && adduser $user_name && adduser $user_name sudo && mkdir /home/$user_name/public_html && chown $user_name:$user_name /home/$user_name && chmod -R 777 /home/$user_name && usermod -d /home/$user_name/public_html $user_name && read -p "Ingresa el dominio del sitio [example.com] " domain_name && echo "<VirtualHost *:80>" >> /etc/apache2/sites-available/$domain_name.conf && echo "ServerName $domain_name" >> /etc/apache2/sites-available/$domain_name.conf && echo "ServerAdmin $user_name@$domain_name" >> /etc/apache2/sites-available/$domain_name.conf && echo "DocumentRoot /home/$user_name/public_html" >> /etc/apache2/sites-available/$domain_name.conf && echo "ErrorLog ${APACHE_LOG_DIR}/error.log" >> /etc/apache2/sites-available/$domain_name.conf && echo "CustomLog ${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-available/$domain_name.conf && echo "</VirtualHost>" >> /etc/apache2/sites-available/$domain_name.conf && a2ensite $domain_name && service apache2 reload && echo "El usuario $user_name se ha creado correctamente"
+clear && read -p "¿Cual es el nombre de usuario que deseas crear? " user_name && 
+adduser $user_name && 
+adduser $user_name sudo && 
+mkdir /home/$user_name/public_html && 
+chown $user_name:$user_name /home/$user_name && 
+chmod 750 /home/$user_name &&
+chmod 755 /home/$user_name/public_html && 
+usermod -d /home/$user_name/public_html $user_name && 
+read -p "Ingresa el dominio del sitio [example.com] " domain_name && 
+echo "<VirtualHost *:80>" >> /etc/apache2/sites-available/$domain_name.conf && 
+echo "ServerName $domain_name" >> /etc/apache2/sites-available/$domain_name.conf && 
+echo "ServerAdmin $user_name@$domain_name" >> /etc/apache2/sites-available/$domain_name.conf && 
+echo "DocumentRoot /home/$user_name/public_html" >> /etc/apache2/sites-available/$domain_name.conf 
+&& echo "ErrorLog ${APACHE_LOG_DIR}/error.log" >> /etc/apache2/sites-available/$domain_name.conf && 
+echo "CustomLog ${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-available/$domain_name.conf && 
+echo "</VirtualHost>" >> /etc/apache2/sites-available/$domain_name.conf && 
+a2ensite $domain_name && 
+service apache2 reload && 
+echo "El usuario $user_name se ha creado correctamente"
